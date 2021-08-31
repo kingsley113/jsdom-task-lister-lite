74decd5786f0858b1320d830f9a4358c6c22f3ce
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-08-30 18:19:43 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,73 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+	const newTaskform = document.getElementById("create-task-form");
+
+	newTaskform.addEventListener("submit", createNewTask);
+
+	// const taskUl = document.getElementById('list');
+	// taskUl.addEventListener("click", (e) => {
+	// 	if (e.target.nodeName === "BUTTON") {
+
+	// 	}
+	// })
+
 });
+
+
+
+function createNewTask(event) {
+	event.preventDefault();
+
+	// new task setup
+	const newTaskText = document.getElementById("new-task-description");
+	const newTask = document.createElement("li");
+
+	newTask.innerText = newTaskText.value;
+
+
+	// createDeleteButton(newTask);
+
+	// append the task to tasks ul
+	appendNewTask(newTask);
+
+	// reset the form
+	event.target.reset();
+};
+
+
+function appendNewTask(task) {
+	document.getElementById("tasks").appendChild(task);
+}
+
+
+function createDeleteButton(task) {
+	const deleteBtn = document.createElement("button");
+
+	deleteBtn.innerHTML = "Yeet this task";
+	deleteBtn.setAttribute('data-description', task.innerText);
+	task.appendChild(deleteBtn);
+	deleteBtn.addEventListener("click", deleteTask(task));
+	// console.log(task);
+}
+
+function deleteTask(task) {
+	task.remove();
+	console.log('task should be yeeted now');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
